8748de890a79c550d4af24d878d7e9433b89ecfa
wadim baslow <wadim.baslow@posteo.de>
2021-12-02 11:34:03 +0100

    hooked the invitation form with api, re-factored the form to handle feedback better, and finally moved the form styles into a mixin

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -5,12 +5,12 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup'
 import axios from 'axios';
 import queryString from "query-string";
+import { Form } from '../style/Mixins';
 
 const SignUp = ({location}) => {
 
-    const [submitted, setSubmitted] = useState(false)
-    const [errorMessage, setErrorMessage] = useState(null)
-    const [completeRegistration, setCompleteRegistration] = useState(false)
+    const [status, setStatus] = useState(null)
+    const [feedback, setFeedback] = useState(null)
     const [registrationName, setRegistrationName] = useState(null)
     const { sessionId } = queryString.parse(location.search);
 
@@ -22,11 +22,12 @@ const SignUp = ({location}) => {
 
     const checkout = async (values, location)=> {
         try {
+            setStatus('forwarding')
             const response = await axios.post("/api/checkout", {values, location})
             window.location = response.data.url;
         } catch (error) {
             console.log(error.message)
-            setErrorMessage('Something went wrong')
+            setStatus('error')
         }
     }
 
@@ -39,18 +40,35 @@ const SignUp = ({location}) => {
                       }
                 })
                 setRegistrationName(response.data.registrationName)
-                setCompleteRegistration(true)
+                setStatus('success')
             } catch (error) {
                 console.log(error.message)
-                setErrorMessage('Something went wrong')
+                setStatus('error')
             }
         }
     
         if (sessionId) {
             verifyPayment(sessionId)
-            setSubmitted(true)
+            setStatus('verify payment')
         }
     },[sessionId])
+
+    useEffect( ()=> {
+        if (status === 'submitting'){
+            setFeedback('Submitting...')
+        } else if (status === 'forwarding'){
+            setFeedback('Forwarding to payment...')
+        } else if (status === 'verify payment') {
+            setFeedback('Quick check...')
+        } else if (status === 'success') {
+            setFeedback(`Success! ${registrationName}, thank you for your registration 🎉.`)
+        } else if (status === 'error'){
+            setFeedback('Oops 😞. Something did not work out.')
+        }
+        else (
+            setFeedback(null)
+        )
+    },[status, registrationName])
 
     const formik = useFormik({
         initialValues: {
@@ -72,7 +90,7 @@ const SignUp = ({location}) => {
         }),
         onSubmit: (values, { resetForm }) => {
             checkout(values, location)
-            setSubmitted(true)
+            setStatus('submitting')
             setTimeout(() => {
                 resetForm()
               }, 400);
@@ -83,52 +101,49 @@ const SignUp = ({location}) => {
         
         <FormContainer>
             {
-                completeRegistration && registrationName ? <p className="confirmation-message">{ registrationName }, thank you for your registration 🎉</p> : 
-                completeRegistration && !registrationName ? <p className="confirmation-message">Thank you for your registration 🎉</p> : 
-                submitted && !errorMessage ?  <p className='submitting-message'>Forwarding you to payment- This may take a moment.</p> :
-                submitted && errorMessage ? <p>{errorMessage}</p> :
-                    <form noValidate onSubmit={formik.handleSubmit}>
-                        <div className="input-container">
-                            <label htmlFor="fullName">Full Name</label>
-                            <input
-                                type="text"
-                                id='fullName'
-                                name='fullName'
-                                placeholder='Jane Doe'
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                value={formik.values.fullName}
-                            />
-                            {formik.touched.fullName &&  formik.errors.fullName ? <p className="error-message">{formik.errors.fullName}</p> : null}
-                        </div>
-                        <div className="input-container">
-                            <label htmlFor="organization">Organization</label>
-                            <input
-                                type="text"
-                                id='organization'
-                                name='organization'
-                                placeholder='Example Ltd.'
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                value={formik.values.organization}
-                            />
-                            { formik.touched.organization && formik.errors.organization ? <p className="error-message">{formik.errors.organization}</p> : null}
-                        </div>
-                        <div className="input-container">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="email"
-                                id='email'
-                                name='email'
-                                placeholder='example@example.com'
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                value={formik.values.email}
-                            />
-                            { formik.touched.email && formik.errors.email ? <p className="error-message">{formik.errors.email}</p> : null}
-                        </div>
-                        <button type='submit' disabled={formik.isSubmitting || !formik.initialErrors} className='submit-button' >Proceed to payment</button>
-                    </form> 
+                feedback ? <p>{feedback}</p> :
+                <form noValidate onSubmit={formik.handleSubmit}>
+                    <div className="input-container">
+                        <label htmlFor="fullName">Full Name</label>
+                        <input
+                            type="text"
+                            id='fullName'
+                            name='fullName'
+                            placeholder='Jane Doe'
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.fullName}
+                        />
+                        {formik.touched.fullName &&  formik.errors.fullName ? <p className="error-message">{formik.errors.fullName}</p> : null}
+                    </div>
+                    <div className="input-container">
+                        <label htmlFor="organization">Organization</label>
+                        <input
+                            type="text"
+                            id='organization'
+                            name='organization'
+                            placeholder='Example Ltd.'
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.organization}
+                        />
+                        { formik.touched.organization && formik.errors.organization ? <p className="error-message">{formik.errors.organization}</p> : null}
+                    </div>
+                    <div className="input-container">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id='email'
+                            name='email'
+                            placeholder='example@example.com'
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.email}
+                        />
+                        { formik.touched.email && formik.errors.email ? <p className="error-message">{formik.errors.email}</p> : null}
+                    </div>
+                    <button type='submit' disabled={formik.isSubmitting || !formik.initialErrors} className='submit-button' >Proceed to payment</button>
+                </form> 
             }
         </FormContainer>
 
@@ -140,81 +155,5 @@ const SignUp = ({location}) => {
 export default SignUp;
 
 const FormContainer = styled.div`
-    padding: 2rem;
-    background-color: #137a757d;
-    border-radius: 1rem;
-    height: fit-content;
-    box-shadow: 
-        0 0.625rem 0.9375rem -0.1875rem rgba(0, 0, 0, 0.1),
-        0 .25rem .375rem -0.125rem rgba(0, 0, 0, 0.05);
-    form {
-        display: grid;
-        grid-row-gap: 1.5rem;
-    }
-    .input-container {
-        display: flex;
-        flex-direction: column;
-    }
-    label {
-        font-weight: 700;
-        margin-bottom: 0.25rem;
-    }
-    input {
-        background: none;
-        background-color: none;
-        border: none;
-        border-bottom: 1px solid #20C9C1;
-        font-size: 1rem;
-        color: #fff;
-        padding: .625rem .625rem .625rem .3125rem;
-        &:focus {
-            outline: none;
-            border: 1px solid #20C9C1;
-            border-radius: 0.5rem;
-        }
-        &::placeholder {
-            color: #1AA19B;
-            font-weight: 400;
-            font-size: 1rem;
-        }
-    }
-    .error-message {
-        font-size: 0.75rem;
-        align-self: flex-end;
-    }
-    .submit-button {
-        all: unset;
-        place-self: end;
-        cursor: pointer;
-        color: #fff;
-        display: inline-flex;
-        appearance: none;
-        -webkit-box-align: center;
-        align-items: center;
-        -webkit-box-pack: center;
-        justify-content: center;
-        user-select: none;
-        white-space: nowrap;
-        vertical-align: middle;
-        outline: transparent solid 2px;
-        outline-offset: 2px;
-        width: fit-content;
-        min-height: 2rem;
-        border-radius: 0.375rem;
-        line-height: 1.2;
-        padding-inline-start: 0.75rem;
-        padding-inline-end: 0.75rem;
-        background-color: #1AA19B;
-        transition: transform 0.3s ease, border 0.3s ease;
-        :active {
-            transform: scale(0.98);
-        }
-        :focus {
-            outline: 1px solid #fff;
-            outline-offset: -4px;
-        }
-        :disabled {
-            cursor: not-allowed;
-        }
-    }
+    ${Form()}
 `
---
+++ src/style/Mixins.js
@@ -0,0 +1,82 @@
+import { css } from 'styled-components'
+
+export const Form = () => css`
+    padding: 2rem;
+    background-color: #137a757d;
+    border-radius: 1rem;
+    height: fit-content;
+    box-shadow: 
+        0 0.625rem 0.9375rem -0.1875rem rgba(0, 0, 0, 0.1),
+        0 .25rem .375rem -0.125rem rgba(0, 0, 0, 0.05);
+    form {
+        display: grid;
+        grid-row-gap: 1.5rem;
+    }
+    .input-container {
+        display: flex;
+        flex-direction: column;
+    }
+    label {
+        font-weight: 700;
+        margin-bottom: 0.25rem;
+    }
+    input {
+        background: none;
+        background-color: none;
+        border: none;
+        border-bottom: 1px solid #20C9C1;
+        font-size: 1rem;
+        color: #fff;
+        padding: .625rem .625rem .625rem .3125rem;
+        &:focus {
+            outline: none;
+            border: 1px solid #20C9C1;
+            border-radius: 0.5rem;
+        }
+        &::placeholder {
+            color: #1AA19B;
+            font-weight: 400;
+            font-size: 1rem;
+        }
+    }
+    .error-message {
+        font-size: 0.75rem;
+        align-self: flex-end;
+    }
+    .submit-button {
+        all: unset;
+        place-self: end;
+        cursor: pointer;
+        color: #fff;
+        display: inline-flex;
+        appearance: none;
+        -webkit-box-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        justify-content: center;
+        user-select: none;
+        white-space: nowrap;
+        vertical-align: middle;
+        outline: transparent solid 2px;
+        outline-offset: 2px;
+        width: fit-content;
+        min-height: 2rem;
+        border-radius: 0.375rem;
+        line-height: 1.2;
+        padding-inline-start: 0.75rem;
+        padding-inline-end: 0.75rem;
+        background-color: #1AA19B;
+        transition: transform 0.3s ease, border 0.3s ease;
+        :active {
+            transform: scale(0.98);
+        }
+        :focus {
+            outline: 1px solid #fff;
+            outline-offset: -4px;
+        }
+        :disabled {
+            cursor: not-allowed;
+        }
+    }
+`
+

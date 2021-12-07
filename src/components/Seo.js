20dbd1210d05902a27bbe1c9924cef3a5775c88c
wadim baslow <wadim.baslow@posteo.de>
2021-12-07 14:43:20 +0100

    added main meta Image

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -26,7 +26,7 @@ const Seo = ({title, description, metaImage, robotParam, author}) => {
         defaultDescription,
         defaultAuthor,
         siteUrl,
-        // defaultImage,
+        defaultImage,
         defaultColor
       } = site.siteMetadata
 
@@ -34,7 +34,7 @@ const Seo = ({title, description, metaImage, robotParam, author}) => {
         title: title ? `${title} | ${defaultTitle}` : defaultTitle,
         description: description || defaultDescription,
         author: author || defaultAuthor,
-        // image: metaImage || defaultImage,
+        image: metaImage || defaultImage,
         color: defaultColor,
         baseUrl: siteUrl,
         // robotParam: robotParam  || 'index, follow'
@@ -51,7 +51,7 @@ const Seo = ({title, description, metaImage, robotParam, author}) => {
           {seo.description && (
             <meta property="og:description" content={seo.description} />
           )}
-          {/* <meta property="og:image" content={seo.image}/> */}
+          <meta property="og:image" content={seo.image}/>
           <meta property="og:url" content={href}/>
           <meta property="og:type" content="website"/>
           
@@ -60,11 +60,11 @@ const Seo = ({title, description, metaImage, robotParam, author}) => {
           {seo.description && (
             <meta name="twitter:description" content={seo.description} />
           )}
-          {/* <meta name="twitter:image" content={seo.image}/> */}
+          <meta name="twitter:image" content={seo.image}/>
 
           <meta itemprop="name" content={site.title}/>
           <meta itemprop="description" content={site.description}/>
-          {/* <meta itemprop="image" content={seo.image}/> */}
+          <meta itemprop="image" content={seo.image}/>
             
           <meta name="theme-color" content={seo.color}/>
           <meta name="msapplication-navbutton-color" content={seo.color}/>

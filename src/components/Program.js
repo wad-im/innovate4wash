e426561e0e5b447d968324109cb15c3924f6b8cb
wadim baslow <wadim.baslow@posteo.de>
2022-01-17 11:03:25 +0100

    refactored program - create conference day

--- src/components/Program.js
+++ src/components/Program.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import {useStaticQuery, graphql} from 'gatsby'
-import SessionCard from './SessionCard';
 import ParticipatingOrganization from './ParticipatingOrganizations';
+import ConferenceDay from './ConferenceDay';
 
 const Program = () => {
 
@@ -49,46 +49,10 @@ const Program = () => {
 
     return ( 
         <ProgramContainer id='program'>
-            <div className="day day-1">
-                <h2>Day 1</h2>
-                <span className="date">27 January 2022</span>
-                <div className="hr" aria-hidden="true"></div>
-                <ol className="program-items">
-                  {
-                    firstDaySessions.map(session => (
-                      <SessionCard key={session.Record_Id} sessionDetails={session}/>
-                    ))
-                  }
-                </ol>
-            </div>
+            <ConferenceDay conferenceDay='Day 1' sessions={firstDaySessions}/>
             <ParticipatingOrganization/>
-            <div className=" day day-2">
-                <h2>Day 2</h2>
-                <span className="date">28 January 2022</span>
-                <div className="hr" aria-hidden="true"></div>
-                <ol className="program-items">
-                  {
-                    secondDaySessions.map(session => (
-                      <SessionCard key={session.Record_Id} sessionDetails={session}/>
-                    ))
-                  }
-                </ol>
-            
-            </div>
-            <div className=" day day-1">
-                <h2>Day 3</h2>
-                <span className="date">29 January 2022</span>
-                <p>Kiwasco will offer technical tours on the third day. More information will follow. Let us know if you'd like to participate.</p>
-                {/* <div className="hr" aria-hidden="true"></div>
-                <ol className="program-items">
-                  {
-                    firstDaySessions.map(session => (
-                      <SessionCard key={session.Record_Id} sessionDetails={session}/>
-                    ))
-                  }
-                </ol> */}
-            </div>
-            
+            <ConferenceDay conferenceDay='Day 2' sessions={secondDaySessions}/>
+            <ConferenceDay conferenceDay='Day 3' />
         </ProgramContainer>
      );
 }
@@ -98,66 +62,4 @@ export default Program;
 const ProgramContainer = styled.section`
     margin-top: 3.052rem;
     
-    .day {
-      overflow: hidden;
-      border-radius: 1rem;
-      padding: 2rem;
-      margin-bottom: 2rem;
-      box-shadow: 
-      0 .625rem .9375rem -0.1875rem rgba(0, 0, 0, 0.1),
-      0 .25rem .375rem -0.125rem rgba(0, 0, 0, 0.05);
-      h2, p, .date {
-          margin-bottom: 16px;
-      }
-    }
-    .program-items {
-      position: relative;
-    }
-    .overlay {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-size: 2.441rem;
-      line-height: 1.3;
-      font-weight: 700;
-      text-align: center;
-    }
-    .day-1 {
-      background: rgb(0,96,92);
-      background: linear-gradient(45deg, rgba(0,96,92,1) 0%, rgba(0,112,108,1) 75%);
-        color: #4DEBEB;
-        .hr {
-          background-color: #4DEBEB;
-          height: 0.05rem;
-          margin-top: 2rem;
-        }
-        .overlay {
-          background: linear-gradient(45deg, rgba(0,96,92,0.9) 0%, rgba(0,112,108,0.9) 75%);
-        }
-    }
-    
-    .day-2 {
-      background: rgb(3,119,119);
-      background: linear-gradient(45deg, rgba(3,119,119,1) 0%, rgba(4,133,133,1) 75%);
-        color: #06EFEF;
-        .hr {
-          background-color: #06EFEF;
-          height: 0.05rem;
-          margin-top: 2rem;
-        }
-        .overlay {
-          background: linear-gradient(45deg, rgba(3,119,119,0.9) 0%, rgba(4,133,133,0.9) 75%);
-        }
-    }
-    .date {
-        display: inline-block;
-        font-size: 1.25rem;
-        line-height: 1.5;
-        font-weight: 700;
-    }
 `
